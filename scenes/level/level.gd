a8f4cc115ae355ba2c4c77b5f335760d9287e01b
Sophie Starkey <sophiestarkeydev@gmail.com>
2025-01-25 14:11:38 +1300

    Add mixing logic and mix button

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -4,8 +4,25 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Globals.cauldron_area2d = $Cauldron/Area2D
+	$Spoon.mix_button_pressed.connect(mix)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+var recipes = {
+	[$Egg, $Ice]: "Egg on the Rocks"
+}
+
+func mix():
+	for recipe in recipes:
+		if not recipe.size() == Globals.current_potion_ingredients.size():
+			continue
+		for ingredient in Globals.current_potion_ingredients:
+			if ingredient not in recipe:
+				continue
+		print(recipes[recipe])
+	
+	# Can't find a recipe, so generate one
