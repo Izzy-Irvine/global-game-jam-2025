4a5738b4743a2e87956aa8cc8d98e6664cd00dbd
Izzy Irvine <18374196+Izzy-Irvine@users.noreply.github.com>
2025-01-26 13:04:11 +1300

    Moved images, and added drink making logic

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -31,7 +31,8 @@ func _on_add_to_cauldron(target: Ingredient):
 
 @onready
 var recipes = {
-	[$Egg, $Ice]: Drink.new("Egg on the Rocks", 99999999),
+	[$Egg, $Ice]: 
+		Drink.new("Egg on the Rocks", 99999999),
 	[$Whisky, $"Lemon Juice", $"Sugar Cube", $"Orange Peel"]: 
 		Drink.new("Whiskey Sour", 1000),
 	[$Whisky, $Bitters, $"Sugar Cube", $"Orange Peel"]: 
@@ -53,15 +54,15 @@ var recipes = {
 	[$Rum, $"Elixir of clarity (secretly vodka)", $"Liquid sunlight (secretly Tequila)", $"Starleaf Bitters", $"Orange liqueur", $"Liquid moonlight (secretly Gin)", $Ice, $Cola]: 
 		Drink.new("Astral Island", 1000),
 	[$"Orange liqueur", $"Fey wine (secretly Prosecco)", $Ice]: 
-		Drink.new("Fey Summer Spritz", 1000),
+		Drink.new("Fey Summer Spritz", 1000, load("res://scenes/level/Fey Summer Spritz.png")),
 	[$"Fey wine (secretly Prosecco)", $"Liquid sunlight (secretly Tequila)"]: 
-		Drink.new("Sunlight Spritz", 1000),
+		Drink.new("Sunlight Spritz", 1000, load("res://scenes/level/Sunlight Spritz.png")),
 	[$"Fey wine (secretly Prosecco)", $"Liquid moonlight (secretly Gin)"]: 
-		Drink.new("Moonlight Spritz", 1000),
+		Drink.new("Moonlight Spritz", 1000, load("res://scenes/level/Moonlight Sprtitz.png")),
 	[$"Starleaf Bitters", $"Fey wine (secretly Prosecco)"]: 
-		Drink.new("Starlight spritz", 1000),
+		Drink.new("Starlight spritz", 1000, load("res://scenes/level/starlight Sprtitz.png")),
 	[$"Liquid moonlight (secretly Gin)", $Egg, $"Lemon Juice", $"Lemon Peel"]: 
-		Drink.new("Moonlight fizz", 1000)	
+		Drink.new("Moonlight fizz", 1000, load("res://scenes/level/Moonlight Fizz.png"))	
 }
 
 var coin_particle_script = load("res://scenes/level/coin_particles.gd")
@@ -76,8 +77,21 @@ func create_sell_effect():
 	coin_particles.global_position = $Cauldron.global_position
 	add_child(coin_particles)
 
+
 func mix():
+	if current_potion_ingredients.size() == 0:
+		return
+	
 	create_sell_effect()
+	var drink = get_drink()
+	drink.apply_to_sprite($Drink)
+	currency += drink.sell_price
+	$Cauldron.visible = false
+	$Drink.visible = true
+	current_potion_ingredients.clear()
+	
+
+func get_drink() -> Drink:
 	for recipe in recipes:
 		if not recipe.size() == current_potion_ingredients.size():
 			continue
@@ -87,8 +101,19 @@ func mix():
 				missing_ingredient = true
 				break
 		if not missing_ingredient:
-			print(recipes[recipe])
-			return
-		
+			return recipes[recipe]
+	
+	if current_potion_ingredients.size() == 1:
+		var ingredient = current_potion_ingredients[0]
+		return Drink.new(ingredient.name, ingredient.costCents * 0.8, ingredient.texture)
+	
+	# generic
+	var sell_price = 0
+	for ingredient in current_potion_ingredients:
+		sell_price += ingredient.costCents
+	sell_price *= 0.8
+	return Drink.new("Mundane Potion", sell_price, null, true)
+	
+	return null
 	
 	# Can't find a recipe, so generate one
