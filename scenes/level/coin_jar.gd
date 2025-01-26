d3762bc783a640950f5d9ac48dd6d5fca82469eb
Sophie Starkey <sophiestarkeydev@gmail.com>
2025-01-26 16:23:30 +1300

    coin jar animation

--- scenes/level/coin_jar.gd
+++ scenes/level/coin_jar.gd
@@ -1,4 +1,8 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 func update_currency(currency: int) -> void:
 	$Label.text = "$%.2f" % (float(currency) / 100.0)
+	if currency >= 100000:
+		frame = 1
+	else:
+		frame = 0
